b98daa2e9095e755162346f820de316e504cbf73
Vighnesh <vighneshshukla00@gmail.com>
2025-04-05 23:12:43 +0530

    vgyujh

--- JavaScript/variabes/index.js
+++ JavaScript/variabes/index.js
@@ -1,5 +1,27 @@
-var age  = 24
+// var age  = 24;
 
-if(true){
-    console.log(age);
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 5;
+//     console.log(age)
+// }
+//    // console.log(age);
+// console.log("Hello World!!!")
+// solve();
+
+// {
+//     let a = 45;
+// }
+// console.log(a);
+
+{
+    let a = 444;
+    console.log(a);
 }
+// console.log(a);
+
+let a = 444;
+
