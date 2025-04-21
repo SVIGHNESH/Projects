f209f4ea44d2251045d1858f223fe754439afc25
Vighnesh <vighneshshukla00@gmail.com>
2025-04-21 21:43:35 +0530

    started the JS

--- JavaScript/variabes/main.js
+++ JavaScript/variabes/main.js
@@ -12,11 +12,35 @@ var myage  = 20;
 console.log(myage);
 
 
-const hisAge = 34;
+const hisAge = 30;
 // hisAge =3;    //reassigning the value in the const is not possible 
 console.log(hisAge);
 
 
-let heraAge;
+let  heraAge;
 
 
+//data types 
+
+const Name = 'SAM';
+const THisage = 33;
+const ratings = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z ;
+console.log(typeof Name);
+console.log(typeof THisage);
+console.log(typeof ratings);
+console.log(typeof isCool);
+console.log(typeof y);
+console.log(typeof z);
+
+
+console.log('My name is ' +  Name  + ' and my age is ' + age )
+
+
+console.log(`My name is ${Name} and age is ${age}`)
+
+const hello = `Hello ${Name} is here!!!`;
+console.log(hello) 
---
+++ JavaScript/variabes/string.js
@@ -0,0 +1,12 @@
+const s = 'hello world!!';
+
+console.log(s.length)
+console.log(s.toLowerCase());
+console.log(s.toUpperCase());
+console.log(s.substring(0,4));
+console.log(s.substring(0,4));
+console.log(s.substring(0,4).toUpperCase());
+
+
+
+
