052dac04a6cde4ad37b940cdadd768f2ef072565
Vighnesh <vighneshshukla00@gmail.com>
2025-04-29 20:36:16 +0530

    something commited i don't know what.

---
+++ JavaScript/variabes/array.js
@@ -0,0 +1,21 @@
+// let numbers = new Array(6);
+// console.log(numbers)
+//
+//
+// const fruits = ['Apple','Banana','Cherry'];
+// fruits.push('Mango'); //adds the mango at the end of the array
+// fruits.unshift('Pear'); //add the Pear at the index 0
+// fruits.pop();
+//
+// console.log(fruits); 
+// console.log(Array.isArray(fruits));
+// console.log(fruits.indexOf('Apple'));
+// console.console.log()
+
+
+person = {
+  firstName : "Harish",
+  lastName :'Chandra',
+  age : 39,
+  experience : ['RDR2','GT]
+}
--- JavaScript/variabes/string.js
+++ JavaScript/variabes/string.js
@@ -8,5 +8,12 @@ console.log(s.substring(0,4));
 console.log(s.substring(0,4).toUpperCase());
 
 
+console.log(s.split('o'));
+
+console.log(s.split(' '));
+
+const str = "technology,TECH,IT,COmputer,Computer,computer";
+console.log(str.split(','))
+
 
 
