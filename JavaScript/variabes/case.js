381af8c78b4287a17b4ba1de17722dab160a469b
Vighnesh <vighneshshukla00@gmail.com>
2025-05-02 00:28:56 +0530

    Just Trying the StufF in the JS

---
+++ JavaScript/variabes/case.js
@@ -0,0 +1,16 @@
+const x= 1;
+const colour = x > 10 ? 'red':'blue';
+
+switch(colour){
+    case 'red':
+        console.log("Red Colour is Selected");
+        break;
+    case 'blue':
+        console.log("Blue is the Colour is it");
+        break;
+  default:
+        console.log("Colour is not red or blue is name ");
+        break;
+} 
+     
+
---
+++ JavaScript/variabes/functions.js
@@ -0,0 +1,4 @@
+const addTwoNums = (num1=23,num2=2) =>return num1+num2; 
+
+console.log(addTwoNums(22,22));
+ 
