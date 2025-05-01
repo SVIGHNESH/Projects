000d9a43709fb3113ac85a36efa5b199103c2bd3
Vighnesh <vighneshshukla00@gmail.com>
2025-05-02 00:37:01 +0530

    ADDES dfeonask

--- JavaScript/variabes/functions.js
+++ JavaScript/variabes/functions.js
@@ -1,4 +1,8 @@
-const addTwoNums = (num1=23,num2=2) =>return num1+num2; 
+//one line funcions 
 
+const addTwoNums = (num1=23,num2=2) =>num1+num2; 
 console.log(addTwoNums(22,22));
- 
+
+const incrm = num1 => ++num1;
+console.log(incrm(43));
+
