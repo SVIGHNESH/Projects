d505b9395e3dc9a9928cc3dcd0863e3cbb4af8b8
Vighnesh <vighneshshukla00@gmail.com>
2025-05-20 23:16:00 +0530

    sldjfsl

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -8,8 +8,31 @@ const userList = document.querySelector('#users');
 myForm.addEventListener('submit',onSubmit);
 
 function onSubmit(e){
-    e.preventDefault;
-    console.log(nameInput);
+    e.preventDefault();
+    // console.log(nameInput.value);
+    if(nameInput.value === '' || emailInput.value === ''){
+        // alert('Please provide the right value')
+        msg.classList.add('error');
+        msg.innerHTML = 'Please Enter All Fields';
+        setTimeout(() => msg.remove(),1500);
+
+    }else{
+        // console.log('success');
+        // msg.innerHTML = '<div style=background:violet>Done </div>';
+
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+
+        //clearing the Values
+        nameInput.value = '';
+        emailInput.value = '';
+        
+
+    }
+    
     
     
 
