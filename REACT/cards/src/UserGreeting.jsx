6789ba7ed89b4821ac7bcc18e88ab2d94086f013
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-03 19:44:43 +0530

    Add prop-types dependency and implement conditional rendering in UserGreeting component with styled messages

--- REACT/cards/src/UserGreeting.jsx
+++ REACT/cards/src/UserGreeting.jsx
@@ -1,17 +1,31 @@
 //Conditional rendering : allows you to control what gets rendered in your application based on certain condiditions
 //                        (show,hide, or change component )
 
+import PropTypes from 'prop-types'
 function UserGreeting(props){
-        
+
             
-                if(props.isLoggedIn){
-                        return <h2>Welcome {props.userName}</h2>
-                }
+                // if(props.isLoggedIn){
+                //         return <h2>Welcome {props.userName} !!</h2>
+                // }
             
-                return <h2>Please Login to Continue</h2>
+                // return <h2>Please Login to Continue !!!</h2>
                 
+        // return(props.isLoggedIn ? <h2 className="welcome-message">Welcome {props.userName}!!!</h2> : <h2 className="login-prompt">Please Login to Continue!!!</h2>);
         
-        
+    const welcomeMessage = <h2                                       className="welcome-message">Welcome {props.userName}!!!</h2>
+    const loginPrompt = 
+    <h2 
+    className="login-prompt">Please Login to Continue!!!
+    </h2>
+
+    return (props.isLoggedIn ? welcomeMessage : loginPrompt)
+}
+
+UserGreeting.proptypes= {
+    isLoggedIn:PropTypes.bool,
+    userName:PropTypes.string
+
 }
 
 export default UserGreeting
