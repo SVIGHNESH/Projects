082120ba3db8a9be926da86fd60f45c079749f92
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-03 22:51:58 +0530

    Refactor List component to use an array of objects for fruits with calories and implement sorting for display

--- REACT/render-list/src/List.jsx
+++ REACT/render-list/src/List.jsx
@@ -1,9 +1,18 @@
 function List(){
 
-    const fruits = ['apple','oranges','bananas','grapes',"mango"];
+    // const fruits = ['apple','oranges','bananas','grapes',"mango"];
+    const fruits = [
+    { id: 1, name: 'apple', calories: 95 },
+    { id: 2, name: 'oranges', calories: 45 },
+    { id: 3, name: 'bananas', calories: 105 },
+    { id: 4, name: 'grapes', calories: 45 },
+    { id: 5, name: "mango", calories: 52 }];
 
+    fruits.sort();
+
+    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name} </li>);
     return(
-        fruits
+       <ol> {listItems}</ol>
 
     );
 }
