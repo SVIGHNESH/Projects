54f366fa202fb1bbf39afa75093a4fea15472f1d
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-03 23:06:01 +0530

    Refactor List component to correct fruit calorie values and enhance sorting logic for better display

--- REACT/render-list/src/List.jsx
+++ REACT/render-list/src/List.jsx
@@ -1,18 +1,31 @@
-function List(){
+function List() {
 
     // const fruits = ['apple','oranges','bananas','grapes',"mango"];
     const fruits = [
-    { id: 1, name: 'apple', calories: 95 },
-    { id: 2, name: 'oranges', calories: 45 },
-    { id: 3, name: 'bananas', calories: 105 },
-    { id: 4, name: 'grapes', calories: 45 },
-    { id: 5, name: "mango", calories: 52 }];
+        { id: 1, name: 'apple', calories: 95 },
+        { id: 2, name: 'oranges', calories: 35 },
+        { id: 3, name: 'bananas', calories: 105 },
+        { id: 4, name: 'grapes', calories: 45 },
+        { id: 5, name: "mango", calories: 52 }];
 
-    fruits.sort();
+  //  fruits.sort((a,b) => a.name.localeCompare(b.name)); //Alphabetical
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name} </li>);
-    return(
-       <ol> {listItems}</ol>
+  //fruits.sort((a,b) => b.name.localeCompare(a.name)); // Reverse Alphabetical
+
+  //  fruits.sort((a,b) => a.calories - b.calories) //Numerica Order
+
+    const lowCalFruits = fruits.filter(fruit => fruit.calories < 100 )
+    
+
+
+
+
+
+
+    fruits.sort((a,b) => b.calories - a.calories)        //Reverse Numeric Order 
+    const listItems = lowCalFruits.map(fruit => <li key={fruit.id}>{fruit.name}: <b>{fruit.calories} </b> </li>);
+    return (
+        <ol> {listItems}</ol>
 
     );
 }
