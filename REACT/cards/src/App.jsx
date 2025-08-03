bc5686227444c9455fc2dc332d51f6dda661991b
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-03 15:32:19 +0530

    ghfkjlk;'; ';'

--- REACT/cards/src/App.jsx
+++ REACT/cards/src/App.jsx
@@ -1,6 +1,6 @@
 // import Card from './Cards.jsx'
 // import Button  from "./Button.jsx";
-import Student  from "./Students";
+import Students from './Students.jsx'
 function App() {
  
 
@@ -10,9 +10,11 @@ function App() {
       <Card/>
       */}
       {/* <Button/> */}
-
-         
-
+      
+        <Students name= {34} age = {34} isStudent={false}/>    
+        <Students name= "Meliodas" age = {10000} isStudent={false}/>
+        <Students name= "THis" age = {8800} isStudent={true}/>
+        <Students name= "That" age = {10} isStudent={true}/>
 
     </>
    
--- REACT/cards/src/Students.jsx
+++ REACT/cards/src/Students.jsx
@@ -3,12 +3,30 @@
 //   < Component key = value /> 
 
 
+//prop-type : a machanism that ensure that the passed value is of the correct datatype
+//         e.g :-     age:PropTypes.number
+
+// import PropTypes from 'prop-types';
+
+
 function Student(props){
+
+
         return(
-            <div>
+            <div className="Students">
                     <p>Name: {props.name}</p>
+                    <p>Age : {props.age}</p>
+                    <p>isStudent : {props.isStudent ? "Yes" : "No"}</p>
             </div>
         );
 }
+
+//proptype begin
+// Student.propTypes = { 
+//         name: PropTypes.string,
+//         age: PropTypes.number,
+//         isStudent: PropTypes.bool
+
+// }
 
 export default Student
